30a77bd9697e0483f2a857d9f795f8f1d3de2f05
onldpeople <42646218+onldpeople@users.noreply.github.com>
2023-10-04 18:24:24 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -139,7 +139,6 @@ return [
 
         'cache' => [
             'scheme' => 'tls',
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
